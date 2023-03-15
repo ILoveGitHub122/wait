7194537739f20a592c68229364e47a4c5a087b8c
ILoveGitHub122 <126015956+ILoveGitHub122@users.noreply.github.com>
2023-03-15 13:14:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function startAnimation() {
 	document.getElementById("btn").style.display = "none";
 	document.getElementById("update").style.display = "block";
 }
+
+const settingsBtn = document.getElementById('settings-btn');
+settingsBtn.addEventListener('click', function() {
+  // visa inställningsmenyn
+});
